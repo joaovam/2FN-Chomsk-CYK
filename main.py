e8a5e473a489ed024d2521b3c5391605f1c7e576
dannielHCVieira <danniel.hcvieira@gmail.com>
2022-12-09 18:05:11 -0300

    Added to2nf

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pprint
 
 import helper
-from Grammar import Grammar, cfgToCnf
+from Grammar import Grammar, cfgToCnf, cfgTo2nf
 from CYK import cyk_parser
 
 
@@ -11,17 +11,18 @@ if __name__ == '__main__':
     g = Grammar()
     g.readGrammar('test')
     #g.print()
-    grammar = cfgToCnf(g)
+    grammar = cfgToCnf(g) # to Chomsky
+    # grammar = cfgTo2nf(g) # to binary form
     #print(grammar)
     g.print()
-    term = helper.findRulesRelatedToTerminals(g)
+    """ term = helper.findRulesRelatedToTerminals(g)
     var = helper.findRulesRelatedToVariables(g)
     print(term)
     print(var)
     sentence_1 = "aaacb"
     isMember = cyk_parser(g, sentence_1)
     print("Is member?", isMember)
-    pprint.pprint(isMember)
+    pprint.pprint(isMember) """
     #print("Pertence:", cyk_alg(terms=term, varies=var, inp=sentence_1))
 
 
